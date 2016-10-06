e3bc2f861887a7e80374598ff91a0b488e48c9d7
John <jjkiely@gmail.com>
2016-10-06 18:23:56 +0100

    some tweaking

--- src/Bot.hs
+++ src/Bot.hs
@@ -2,15 +2,16 @@
 
 module Bot where
 
-import Data.ByteString
-import System.Environment
+import           Data.ByteString
+import           Data.Monoid
+import           System.Environment
 import qualified Data.ByteString.Char8 as B8
-import qualified Data.ByteString.Lazy as BL
-import qualified Network.HTTP.Base as HTTP
-import Network.HTTP.Client
-import Network.HTTP.Client.TLS
-import Network.HTTP.Types
-import Web.Authenticate.OAuth
+import qualified Data.ByteString.Lazy  as BL
+import qualified Network.HTTP.Base     as HTTP
+import           Network.HTTP.Client
+import           Network.HTTP.Client.TLS
+import           Network.HTTP.Types
+import           Web.Authenticate.OAuth
 
 getEnv' = (B8.pack <$>) . getEnv
 
@@ -18,7 +19,7 @@ oauthTwitter :: IO OAuth
 oauthTwitter = do
   apiKey <- getEnv' "OAUTH_ACCESS_TOKEN"
   apiSecret <- getEnv' "OAUTH_ACCESS_SECRET"
-  let oauth =newOAuth { oauthServerName      = "twitter"
+  let oauth = newOAuth { oauthServerName      = "twitter"
            , oauthRequestUri      = "https://api.twitter.com/oauth/request_token"
            , oauthAccessTokenUri  = "https://api.twitter.com/oauth/access_token"
            , oauthAuthorizeUri    = "https://api.twitter.com/oauth/authorize"
@@ -38,7 +39,8 @@ signAuth request = do
 
 tweet :: String -> IO (Response BL.ByteString)
 tweet status = do
-  url <- parseUrl $ "https://api.twitter.com/1.1/statuses/update.json?status=" ++ HTTP.urlEncode status
+  url <- parseUrl $ "https://api.twitter.com/1.1/statuses/update.json?status=" `Data.Monoid.mappend` (HTTP.urlEncode status)
+  print url
   req <- signAuth url{ method = "POST" }
   manager <- newManager tlsManagerSettings
   httpLbs req manager
